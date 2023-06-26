66aaee5c96aa68566dc492a5e49954dcf11d286f
mishota <mishota.o@gmail.com>
2023-06-26 23:16:36 +0300

    tooltip

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,33 @@
+"use client"
 import Link from "next/link";
-import styles from "./About.module.scss"
+import styles from "./About.module.scss";
+import { Tooltip } from "@mui/material";
 
 const getData = async () => {
-    const ids = [1, 2, 3, 4, 5];
-    return ids;
-}
+  const ids = [1, 2, 3, 4, 5];
+  return ids;
+};
 
 const About = async () => {
-    const ids = await getData();
-    return (
-        <div className={styles.container}>
-            <div className={styles.title}><h1>ABOUT</h1></div>
-            <div className={styles.links}>
-                {ids.map(elem => (
-                    <div key={elem}>
-                        <Link href={`/about/${elem}`}>{elem}</Link>
-                    </div>
-                ))}
-            </div>
+  const ids = await getData();
+  return (
+    <div className={styles.container}>
+      <Tooltip title="About">
+        <div className={styles.title}>
+          <h1>ABOUT</h1>
         </div>
-    )
-}
+        {/* <IconButto>
+          <DeleteIcon />
+        </IconButto> */}
+      </Tooltip>
+      <div className={styles.links}>
+        {ids.map((elem) => (
+          <div key={elem}>
+            <Link href={`/about/${elem}`}>{elem}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default About;
